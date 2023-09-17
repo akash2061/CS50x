76f93eb5aa5b72814bd9e9742780f2a975e5b6f0
akash2061 <lucifermorningstar2061@gmail.com>
2023-09-17 19:10:20 +0530

    readability

---
+++ readability/readability.c
@@ -0,0 +1,78 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+    int index;
+    float L,S;
+
+    L = ((float) letters / words) * 100;
+    S = ((float) sentences / words) * 100;
+    index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %d\n", index);
+    }
+
+    return 0;
+}
+
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(string text)
+{
+    int count = 1;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isspace(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
+}
